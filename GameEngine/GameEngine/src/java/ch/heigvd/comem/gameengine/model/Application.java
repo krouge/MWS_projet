9255018a792b4272348517285351723c7cc12de1
Julien Biedermann <julien.biedermann@gmail.com>
2013-03-15 13:25:38 +0100

    Ajout classe Application

--- GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Application.java
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Application.java
@@ -1,14 +1,63 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ch.heigvd.comem.gameengine.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author Julien Biedermann
  */
-public class Application {
+@Entity
+public class Application implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+    private String description;
+    
+    private String apiKey;
+    
+    private String apiSecret;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Application)) {
+            return false;
+        }
+        Application other = (Application) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ch.heigvd.comem.gameengine.model.Application[ id=" + id + " ]";
+    }
 
 }
