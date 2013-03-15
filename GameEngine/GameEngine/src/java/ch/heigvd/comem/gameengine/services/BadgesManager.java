eb7502e534221e71c8388721da876a9ccd6ed1c7
Julien Biedermann <julien.biedermann@gmail.com>
2013-03-15 14:36:15 +0100

    Changement

--- GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/services/BadgesManager.java
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/services/BadgesManager.java
@@ -2,17 +2,23 @@ package ch.heigvd.comem.gameengine.services;
 
 import ch.heigvd.comem.gameengine.model.Badge;
 import javax.ejb.Stateless;
+import javax.jws.WebService;
 
 /**
  *
  * @author julien
  */
 @Stateless
+@WebService
 public class BadgesManager implements BadgesManagerLocal {
 
     @Override
     public Badge createBadge(String name, String descrition, String source) {
-        return null;
+        Badge badge = new Badge();
+        badge.setName(name);
+        badge.setDescription(descrition);
+        
+        return badge;
     }
 
     
