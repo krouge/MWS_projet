e70b640f09246fd191394b657692b6aced3909ce
Renouille <Renouille@GROSSMR1.einet.ad.eivd.ch>
2013-03-15 15:41:58 +0100

    rule manager

---
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/services/EventsManagerLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.comem.gameengine.services;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Renouille
+ */
+@Stateless
+public class EventsManagerLocal implements EventsManagerLocalLocal {
+
+    
+}
