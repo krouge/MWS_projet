c5bc5a12c4aacf88f88cf31e56d67d9bf064c764
Renouille <Renouille@GROSSMR1.einet.ad.eivd.ch>
2013-03-15 13:12:56 +0100

    Création classe Rule.java

---
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Rule.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.comem.gameengine.model;
+
+/**
+ *
+ * @author Renouille
+ */
+public class Rule {
+    
+}
