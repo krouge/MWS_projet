0922fb68358ac2bc28da7eea52c826c261d12092
Renouille <Renouille@GROSSMR1.einet.ad.eivd.ch>
2013-03-15 14:33:02 +0100

    Rule class

--- GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Rule.java
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Rule.java
@@ -4,10 +4,67 @@
  */
 package ch.heigvd.comem.gameengine.model;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
 /**
  *
  * @author Renouille
  */
-public class Rule {
+@Entity
+public class Rule implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @NotNull
+    private String eventType;
+    
+    @NotNull 
+    private int numberOfPoints;
+    
+    @ManyToOne
+    private Badge badge;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ruleId;
+
+    public Long getId() {
+        return ruleId;
+    }
+
+    public void setRuleId(Long id) {
+        this.ruleId = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (ruleId != null ? ruleId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the ruleId fields are not set
+        if (!(object instanceof Rule)) {
+            return false;
+        }
+        Rule other = (Rule) object;
+        if ((this.ruleId == null && other.ruleId != null) || (this.ruleId != null && !this.ruleId.equals(other.ruleId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ch.heigvd.comem.gameengine.model.Rule[ id=" + ruleId + " ]";
+    }
     
 }
