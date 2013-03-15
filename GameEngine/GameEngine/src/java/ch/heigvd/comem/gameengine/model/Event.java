971320df2d2c81e80090f91ea5630c74bb1aa0b3
Renouille <Renouille@GROSSMR1.einet.ad.eivd.ch>
2013-03-15 15:00:48 +0100

    Event + Rule class

--- GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Event.java
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/model/Event.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -20,9 +22,42 @@ public class Event implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @ManyToOne
+    private Application application;
+    
+    @ManyToOne
+    private Player player;
 
+    @NotNull
+    private String eventType;
+    
     public Long getId() {
         return id;
+    }
+
+    public Application getApplication() {
+        return application;
+    }
+
+    public void setApplication(Application application) {
+        this.application = application;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
     }
 
     public void setId(Long id) {
