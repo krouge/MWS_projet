b13cf3749583a5ff50af4f64ec013d5fd8198fac
Julien Biedermann <julien.biedermann@gmail.com>
2013-03-18 16:28:49 +0100

    Changements pire cool et justes

--- GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/rest/BadgeFacadeREST.java
+++ GameEngine/GameEngine/src/java/ch/heigvd/comem/gameengine/rest/BadgeFacadeREST.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.heigvd.comem.gameengine.rest;
 
 import ch.heigvd.comem.gameengine.model.Badge;
